47a1b07f56646a123aa09e43fe37080ba350569d
MaxMck <mm.moock@gmail.com>
2014-10-08 21:03:31 +0200

    option1done

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -1,31 +1,52 @@
 def old_roman_numeral(an_integer)
-  # TODO: translate integer in roman number - old-style way
-chiffre = an_integer
-m = an_integer / 1000
-chiffre -= m * 1000
-d = chiffre / 500
-chiffre -= d * 500
-c = chiffre / 100
-chiffre -= c * 100
-l = chiffre / 50
-chiffre -= l * 50
-x = chiffre / 10
-chiffre -= x * 10
-v = chiffre / 5
-chiffre -= v * 5
-i = chiffre
+  chiffre = an_integer
+  m = an_integer / 1000
+  chiffre -= m * 1000
+  d = chiffre / 500
+  chiffre -= d * 500
+  c = chiffre / 100
+  chiffre -= c * 100
+  l = chiffre / 50
+  chiffre -= l * 50
+  x = chiffre / 10
+  chiffre -= x * 10
+  v = chiffre / 5
+  chiffre -= v * 5
+  i = chiffre
 
-message = ""
-message += "M" * m
-message += "D" * d
-message += "C" * c
-message += "L" * l
-message += "X" * x
-message += "V" * v
-message += "I" * i
-
+  message = ""
+  message += "M" * m
+  message += "D" * d
+  message += "C" * c
+  message += "L" * l
+  message += "X" * x
+  message += "V" * v
+  message += "I" * i
 end
 
 def new_roman_numeral(an_integer)
-  # TODO: translate integer in roman number - modern-style way
+  resultat = old_roman_numeral(an_integer)
+
+  if resultat.include?("VIIII")
+    resultat = resultat.sub("VIIII", "IX")
+  elsif resultat.include?("IIII")
+    resultat = resultat.sub("IIII", "IV")
+  end
+
+  if resultat.include?("XXXX")
+    resultat = resultat.sub("XXXX", "XL")
+  elsif resultat.include?("LXXXX")
+    resultat = resultat.sub("LXXXX", "XC")
+  end
+
+  resultat = resultat.sub("LXL", "XC") if resultat.include?("LXL")
+
+  if resultat.include?("DCCCC")
+    resultat = resultat.sub("DCCCC", "CM")
+  elsif resultat.include?("CCCC")
+    resultat = resultat.sub("CCCC", "CD")
+  end
+
+  puts "#{resultat}"
+  resultat
 end
