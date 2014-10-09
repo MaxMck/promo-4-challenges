c535d7e87339bfd29e92925634addf090d03bea9
MaxMck <mm.moock@gmail.com>
2014-10-09 12:33:57 +0200

    1stpart

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,13 +1,6 @@
 def shuffle_word(a_word)
   # TODO: refactor this method
-
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
-
-  shuffle_array
-
+ a_word.upcase.chars.to_a.shuffle
 end
 
 
