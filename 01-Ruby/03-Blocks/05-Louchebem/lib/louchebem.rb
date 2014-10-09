28e9a373fc01e5c88914e45d9274a84493e477f4
MaxMck <mm.moock@gmail.com>
2014-10-09 17:16:08 +0200

    ex5

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,24 @@
 # Encoding: utf-8
+def louchebemize_word(word)
+  vowel = ["a", "e", "i", "o", "u"]
+  suffix = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+  if word.size == 1
+    word
+  else
+    until vowel.include? (word[0])
+      word << word[0]
+      word.reverse!.chop!.reverse!
+    end
+    word.insert(0, "l")
+    word << suffix[rand(7)]
+  end
+word
+end
 
 def louchebemize(sentence)
-  # TODO: implement your louchebem translator
+ array = sentence.split(/\W+/)
+ array.each { |x| louchebemize_word(x) }
+ array.join(" ")
 end
+
+print louchebemize("le chat est mignon!")
