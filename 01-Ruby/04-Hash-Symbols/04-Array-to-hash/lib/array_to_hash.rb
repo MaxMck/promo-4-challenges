b19c31bcf599fe5470a0ba36beb4a2c806ee641e
MaxMck <mm.moock@gmail.com>
2014-10-10 16:36:29 +0200

    ex4

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,7 +1,10 @@
 def array_to_hash(array)
   #TODO: implement the method :)
-hash = Hash.new
-array.each_with_index {|item, index| hash[index] = item}
-hash
-end
 
+  hash = Hash.new
+  array.each_with_index {|item, index|
+    index = yield(index) if block_given?
+    hash[index] = item
+   }
+  hash
+end
