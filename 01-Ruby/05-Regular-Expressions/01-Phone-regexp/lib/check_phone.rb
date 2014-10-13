66fcba0708da048338e38292a002bf415bf13195
MaxMck <mm.moock@gmail.com>
2014-10-13 11:37:22 +0200

    ex1

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,11 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  tel = phone_number.tr_s(" ", "").tr_s("-", "")
+  if tel =~ (/^0[^0]([0-9]{8})$/)
+    true
+  elsif tel =~ (/^\W33([0-9]{9})$/)
+    true
+  else
+    false
+  end
 end
