1e038c3eb8038dd57ca6033ace2d06807c48d05a
MaxMck <mm.moock@gmail.com>
2014-10-13 16:36:00 +0200

    ex3b

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,21 +1,16 @@
 # Encoding: utf-8
 
 def mail_joke(email)
-  # TODO: Return (not print!) a joke suited to the email provided
   match_data = email.match(/^(\S*)@(\w+).(\S*)$/)
-  raise ArgumentError.new("Enter a correct email adress") unless match_data
+  fail ArgumentError.new("Enter a correct email adress") unless match_data
   prenomnom = match_data[1].split(".")
-  prenom = prenomnom[0].to_s
-  nom = prenomnom[1].to_s
   if match_data[2] == "lewagon"
-    p "Well done #{prenom}, you're skilled and capable"
+    p "Well done #{prenomnom[0]}, you're skilled and capable"
   elsif match_data[2] == "gmail"
-    p "#{prenom}, you're an average but modern person"
+    p "#{prenomnom[0]}, you're an average but modern person"
   elsif match_data[2] == "live"
-    p "#{prenom} #{nom}, aren't you born before 1973?"
+    p "#{prenomnom[0]} #{prenomnom[1]}, aren't you born before 1973?"
   else
     return "Sorry #{match_data[1]}, we don't know how to judge '#{match_data[2]}.#{match_data[3]}'"
   end
 end
-
-mail_joke("max.moock@kiwi.com")
