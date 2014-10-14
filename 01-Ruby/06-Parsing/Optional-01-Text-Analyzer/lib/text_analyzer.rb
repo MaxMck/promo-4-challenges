2afe6722e59280c629e15397efabe8efa20990c9
MaxMck <mm.moock@gmail.com>
2014-10-14 17:33:39 +0200

    ex3

--- 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -1,3 +1,17 @@
 def analyze(text)
-  # TODO: should analyze the text, and return the result hash with all features
+  character = text.split('').size
+  line = text.split("\n").size
+  paragraph = text.split(/\n{2}/).size
+  sentence = text.split(/[.!?;]/).size
+  word = text.gsub(/[^\w\s]/, "").split(" ").size
+  if sentence == 0 || paragraph == 0
+    return { character_count: character, line_count: line, word_count: word,
+    sentence_count: sentence, paragraph_count: paragraph,
+    average_words_per_sentence: 0, average_sentences_per_paragraph: 0 }
+  else
+    return { character_count: character, line_count: line, word_count: word,
+    sentence_count: sentence, paragraph_count: paragraph,
+    average_words_per_sentence: (word / sentence), average_sentences_per_paragraph: (sentence / paragraph) }
+  end
 end
+
