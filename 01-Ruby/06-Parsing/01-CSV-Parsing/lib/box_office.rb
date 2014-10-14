19e1661c9705e094b4104e22f296baa4f6351dc7
MaxMck <mm.moock@gmail.com>
2014-10-14 11:34:15 +0200

    ex1b

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -8,7 +8,7 @@ def most_successfull(number, max_year, file_name)
 
   movie_array = []
   CSV.foreach(filepath, csv_options) do |row|
-    movie_hash = { :name => row[0], :year => row[1].to_i, :earnings => row[2].to_i }
+    movie_hash = { name: row[0], year: row[1].to_i, earnings: row[2].to_i }
     movie_array << movie_hash if movie_hash[:year].to_i < max_year
   end
   movie_array.sort_by { :earnings }
