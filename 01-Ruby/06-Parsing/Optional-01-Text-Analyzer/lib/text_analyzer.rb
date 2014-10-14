ed7fe5e0ddf743ef71a319bd0429ed66ea1a90de
MaxMck <mm.moock@gmail.com>
2014-10-14 17:44:52 +0200

    ex3fin

--- 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -4,14 +4,10 @@ def analyze(text)
   paragraph = text.split(/\n{2}/).size
   sentence = text.split(/[.!?;]/).size
   word = text.gsub(/[^\w\s]/, "").split(" ").size
-  if sentence == 0 || paragraph == 0
-    return { character_count: character, line_count: line, word_count: word,
-    sentence_count: sentence, paragraph_count: paragraph,
-    average_words_per_sentence: 0, average_sentences_per_paragraph: 0 }
-  else
-    return { character_count: character, line_count: line, word_count: word,
-    sentence_count: sentence, paragraph_count: paragraph,
-    average_words_per_sentence: (word / sentence), average_sentences_per_paragraph: (sentence / paragraph) }
-  end
+  avg_words_per_sentence = sentence.zero? ? 0 : (word / sentence)
+  avg_sentences_per_paragraph = paragraph.zero? ? 0 : (sentence / paragraph)
+  return { character_count: character, line_count: line, word_count: word,
+  sentence_count: sentence, paragraph_count: paragraph,
+  average_words_per_sentence: avg_words_per_sentence, average_sentences_per_paragraph: avg_sentences_per_paragraph }
 end
 
