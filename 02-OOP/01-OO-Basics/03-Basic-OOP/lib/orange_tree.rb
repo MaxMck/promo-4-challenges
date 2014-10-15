2cdae1f33b100fa908edec19b2319b0710e179ef
MaxMck <mm.moock@gmail.com>
2014-10-15 12:38:25 +0200

    ex3

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,36 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+  attr_accessor :age, :height, :fruits
+
+  def initialize
+    @height = 0
+    @age = 0
+  end
+
+  def one_year_passes!
+    @age += 1
+    @height += 1 if @age <= 10
+    if @age <= 5
+      @fruits = 0
+    elsif @age <= 10
+      @fruits = 100
+    elsif @age <= 15
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+  end
+
+  def dead?
+    if @age < 50
+      return false
+    elsif @age - 50 < (1..50).to_a.sample
+      return false
+    else
+      return true
+    end
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1
+  end
 end
