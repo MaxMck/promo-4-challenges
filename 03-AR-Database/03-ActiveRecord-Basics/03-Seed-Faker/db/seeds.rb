bde5466b12abb2b4330b625ada8f48301c905512
MaxMck <mm.moock@gmail.com>
2014-10-27 15:43:46 +0100

    ex3

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,8 @@
 require "faker"
-
-# TODO: Write a seed to insert 100 posts in the database
+100.times do
+  name = Faker::Internet.user_name
+  url = Faker::Internet.url
+  votes = Faker::Number.digit
+  p = Post.create(name: name, url: url, votes: votes)
+  p.save
+end
