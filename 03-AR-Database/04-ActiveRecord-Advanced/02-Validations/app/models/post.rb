2f35e63d2ebf1f40211700e4902a2fad23690d04
MaxMck <mm.moock@gmail.com>
2014-10-28 12:25:48 +0100

    ex2

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
-  # TODO: Add some validation
+  validates_presence_of :name, :url, :user
+  validates :url, format: { with:
+    /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/, message: "invalid url" }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, length: { minimum: 5 }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
-
-  # TODO: Add some validation
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/, message: "invalid email" }
 end
