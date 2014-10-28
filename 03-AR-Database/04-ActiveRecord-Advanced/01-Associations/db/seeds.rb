d7fec21e5c6af489bd13e4626e71505b0eee68ff
MaxMck <mm.moock@gmail.com>
2014-10-28 11:27:32 +0100

    ex1

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,13 @@
-# TODO: Write a seed
+require "faker"
+5.times do
+  username = Faker::Internet.user_name
+  email = Faker::Internet.email
+  user = User.create(username: username, email: email)
+  user.save
+  8.times do
+    name = Faker::Internet.user_name
+    url = Faker::Internet.url
+    votes = Faker::Number.digit
+    user.posts.create(name: name, url: url, votes: votes)
+  end
+end
