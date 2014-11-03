3f5d233e08ea078e07d0d9d5fbd91d47d8f7973f
MaxMck <mm.moock@gmail.com>
2014-11-03 11:42:32 +0100

    4

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -22,7 +22,7 @@ function run_challenges() {
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $('th').addClass('blue')
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
